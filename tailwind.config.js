4568820df312c3240a01f6bb20205fd30f579d3b
Hriday Jain <hridaymjain@gmail.com>
2024-04-27 15:33:28 +0530

    Creating Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'gray-darkest': '#202020',
+        'gray-dark': '#4d4d4d',
+        'yellow-light': '#ffd100',
+        'yellow-dark': '#ffee32',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
