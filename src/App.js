98c3e9aa697b9c7d9e818f1e0a7072f70b932b0b
Hriday Jain <hridaymjain@gmail.com>
2024-04-27 16:35:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import ProjectsPage from './ProjectsPage';
 import ContactPage from './ContactPage';
@@ -7,11 +7,9 @@ import ContactPage from './ContactPage';
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/projects" component={ProjectsPage} />
-        <Route path="/contact" component={ContactPage} />
-      </Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/projects" component={ProjectsPage} />
+      <Route path="/contact" component={ContactPage} />
     </Router>
   );
 };
